4a450b39cfa01c11713d149e15afa8c85cef6965
Muhammad Sahal Qasim <s.m.sahal786@outlook.com>
2023-03-29 01:53:12 +0500

    second audio

--- js/voice.js
+++ js/voice.js
@@ -3,3 +3,9 @@ var vselect = $("#voiceselection");
 $.each(voiceselection, function() {
         vselect.append($("<option />").val(this.name).text(this.name));
 });
+
+
+function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
